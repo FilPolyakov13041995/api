d7d6de871cb8d6a1adf304d114c16fe44caf6b29
Filipp <Filipp-p@mail.ru>
2024-09-22 03:46:57 +0500

    add env

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ const server = new ApolloServer({ typeDefs, resolvers, introspection: true, play
 const app = express();
 server.start().then(() => {
     server.applyMiddleware({ app });
-    app.listen({ port: 4000 }, () => {
-        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    app.listen({ port: process.env.PORT || 4000 }, () => {
         console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
     });
 });
