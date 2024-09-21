5d4be49ba061a3676e1da68f6ce4729d3f1c47b5
Filipp <Filipp-p@mail.ru>
2024-09-22 02:47:03 +0500

    api

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type User {
+        name: String
+        age: Int
+        address: String
+    }
+
+    type Query {
+        users: [User]
+    }
+`; 
+
+const users = [
+    {
+        name: "Helen Adams",
+        age: 29,
+        address: "123 Main St"
+    },
+    {
+        name: "Phil Adams",
+        age: 24,
+        address: "456 Elm St"
+    }
+];
+
+const resolvers = {
+    Query: {
+        users: () => users
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.start().then(() => {
+    server.applyMiddleware({ app });
+    app.listen({ port: 4000 }, () => {
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    });
+});
