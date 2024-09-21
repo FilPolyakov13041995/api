eb655079d20ef7fad587528474394ef8a5b47432
Filipp <Filipp-p@mail.ru>
2024-09-22 03:16:09 +0500

    Enable introspection and playground

--- index.js
+++ index.js
@@ -32,7 +32,14 @@ const resolvers = {
     }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer(
+    {
+        typeDefs,
+        resolvers,
+        introspection: true,
+        playground: true
+    }
+);
 
 const app = express();
 server.start().then(() => {
