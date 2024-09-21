284765396e0c7960d5675cc3608dd211309a7b5e
Filipp <Filipp-p@mail.ru>
2024-09-22 03:35:42 +0500

    end

--- index.js
+++ index.js
@@ -39,5 +39,6 @@ server.start().then(() => {
     server.applyMiddleware({ app });
     app.listen({ port: 4000 }, () => {
         console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
     });
 });
